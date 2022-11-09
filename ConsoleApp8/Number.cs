7df4ac17ed75dd22507e0b91d52d43081322b4ee
kira <kira@mail.ru>
2022-11-09 21:00:40 +0300

    resosa

---
+++ ConsoleApp8/Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+   public  class Number
+    {
+        public double Nmbr(double firstNumber)
+        {
+        int degree = 2;
+               Console.WriteLine(Math.Pow(firstNumber, degree));              
+               Console.ReadKey();
+            return firstNumber;
+        }
+        public double NmbrSqrt(double firstNumber)
+        {
+            Console.WriteLine(Math.Sqrt(firstNumber));
+            Console.ReadKey();
+            return firstNumber;
+        }
+    }
+}
---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            Console.WriteLine(number.Nmbr(22));
+            Console.WriteLine(number.NmbrSqrt(144));
+        }
+    }
+}
